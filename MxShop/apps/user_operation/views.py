5766089a3d3da58cdb51ee6c7db877d52ced5d25
yangzhe <771874135@qq.com>
2019-07-16 17:03:19 +0800

    优化用户收藏功能和vue的联调

--- MxShop/apps/user_operation/views.py
+++ MxShop/apps/user_operation/views.py
@@ -10,7 +10,8 @@ from utils.permissions import IsOwnerOrReadOnly
 
 
 class UserFavViewset(mixins.CreateModelMixin, mixins.DestroyModelMixin,
-                     viewsets.GenericViewSet, mixins.ListModelMixin):
+                     viewsets.GenericViewSet, mixins.ListModelMixin,
+                     mixins.RetrieveModelMixin):
     """
     用户收藏功能
     """
@@ -20,6 +21,8 @@ class UserFavViewset(mixins.CreateModelMixin, mixins.DestroyModelMixin,
     # 如果放到settings中就会做全局的认证
     # 配置SessionAuthentication之后drf后台才可以登录
     authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
+    # 修改默认搜索字段
+    lookup_field = "goods_id"
 
     # 然后需要重载这个方法。只返回当前用户的收藏信息
     def get_queryset(self):
